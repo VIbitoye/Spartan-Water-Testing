220379ab5bb3f769a5b7d3f96014695706d97f72
VIbitoye <victoribitoye163@gmail.com>
2024-11-08 16:53:42 -0500

    Removed deprecated code

--- spartan-water-testing-backend/index.js
+++ spartan-water-testing-backend/index.js
@@ -35,13 +35,8 @@ app.use('/api/orders', orderRoutes);
 app.use('/api/kits', waterKitRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Error connecting to MongoDB:', error));
 
 // Start the server
